3014645e36774acd6af57b2b0ab7132ef13561bd
Hiroki Kato <hkato@sas.upenn.edu>
2018-02-12 06:12:20 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  i = 0
+    while i < array.length
+      yield(array[i])
+        i = i + 1
+      end
+    array
+  end
+
+test_array = [2, 4, 6, 8]
+
+my_each(test_array) do |x|
+  x + 1
 end
